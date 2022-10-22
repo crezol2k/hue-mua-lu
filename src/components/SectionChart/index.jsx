ad31dfab1af3a2e10644a59eb08b3eef0b27343a
Van Dang <nguyenvandang19122000@gmail.com>
2022-10-22 18:44:14 +0700

    develop: update code finish project

--- src/components/SectionChart/index.jsx
+++ src/components/SectionChart/index.jsx
@@ -1,98 +1,144 @@
-import React from "react";
-import "./style.css";
 import {
-  Chart as ChartJS,
   CategoryScale,
+  Chart as ChartJS,
+  Filler,
+  Legend,
   LinearScale,
-  BarElement,
+  LineElement,
+  PointElement,
   Title,
   Tooltip,
-  Legend,
 } from "chart.js";
-import { Bar } from "react-chartjs-2";
+import React from "react";
+import "./style.css";
 import ScrollAnimation from "react-animate-on-scroll";
+
+import Chart from "react-apexcharts";
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
-  BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
+  Filler,
   Legend
 );
 
-const SectionChart = (props) => {
+const SectionChart = () => {
+  const waters = [375, 275, 372, 369, 368, 381, 395, 400];
   const labels = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
+    "1h00",
+    "2h00",
+    "3h00",
+    "4h00",
+    "5h00",
+    "6h00",
+    "7h00",
+    "8h00",
   ];
 
   const options = {
     animation: {
       duration: 2,
     },
-    responsive: true,
-    plugins: {
-      legend: {
-        display: true,
-        position: "bottom",
-        lineDashOffset: 40,
-        labels: {
-          boxHeight: 10,
-          boxWidth: 10,
-        },
-      },
-      datalabels: {
-        display: labels.map((item) => item),
-        color: "black",
-        align: "end",
-        anchor: "end",
+    title: {
+      text: 'Sông Hương: ngày 15/10/2022',
+      align: 'center',
+      style: {
+        fontSize: '14px'
+      }
+    },
+    labels: labels,
+    chart: {
+      toolbar: {
+        show: false,
       },
     },
-  };
-
-  const data = {
-    labels: props.labels,
-    datasets: [
-      {
-        label: "Tháng 10 trung bình nhiều năm",
-        data: 10,
-        backgroundColor: "rgb(229, 189, 95);",
+    dataLabels: {
+      enabled: false,
+    },
+    colors: ["#023e8a", "#90e0ef"],
+    stroke: {
+      width: 1,
+      curve: "smooth",
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shadeIntensity: 1,
+        opacityFrom: 0.7,
+        opacityTo: 0.9,
+        colorStops: [
+          {
+            offset: 0.1,
+            color: "#8ecae6",
+            opacity: 0.8,
+          },
+          {
+            offset: 100,
+            color: "white",
+            opacity: 1,
+          },
+        ],
+        stops: [0, 45, 85, 100],
       },
+    },
+    yaxis: [
       {
-        label: "Từ 1- 20/10 trung bình nhiều năm",
-        data: 20,
-        backgroundColor: "rgb(135, 89, 43)",
-      },
-      {
-        label: "Từ 1- 20/10/2020",
-        data: 30,
-        backgroundColor: "rgb(255, 179, 0)",
+        show: true,
+        min: 0,
+        max: 500,
+        labels: {
+          show: true,
+          style: {
+            colors: "#A9A9A9",
+            fontSize: "12px",
+            fontWeight: 400,
+            cssClass: "apexcharts-yaxis-label",
+          },
+          offsetX: 0,
+          offsetY: 0,
+          rotate: 0,
+          formatter: function (value) {
+            return `${value} cm`;
+          },
+        },
       },
     ],
   };
+  const series = [
+    {
+      name: "Mực nước",
+      data: waters,
+    },
+  ];
   return (
-    <>
+    <div className="d-flex justify-content-center">
       <div className="container section-1">
         <ScrollAnimation animateIn="animate__lightSpeedInLeft">
           <div
-            style={{ width: "800px", margin: "auto auto" }}
-            className="pt-5 pb-3"
+            className="pt-5 pb-3 position-relative"
+            style={{ width: "800px", margin: "0 auto" }}
           >
-            <Bar options={options} data={data} />
+            <Chart
+              options={options}
+              type="area"
+              series={series}
+              // width={800}
+              // height={400}
+            />
+            <div className="line-warning1"></div>
+            <div className="line-warning2"></div>
+            <div className="line-warning3"></div>
             <p className="mt-2" style={{ fontWeight: "bold" }}>
               Đô thị kiểu mẫu cũng ngập.
             </p>
           </div>
         </ScrollAnimation>
       </div>
-    </>
+    </div>
   );
 };
-
 export default SectionChart;
